edbdbf9ff15f40509a8c459f4646e072d05465cc
rafifadhila440 <rafifadhila440@gmail.com>
2022-11-13 14:32:49 +0700

    def penjumlahan dan pengurangan

--- aplikasi_matriks_PT10.py
+++ aplikasi_matriks_PT10.py
@@ -4,7 +4,7 @@
 #Penjumlahan
 import os
 def penjumlahan():
-    #print("=" * 90)    
+    #print("=" * 60)    
     print("Penjumlahan matriks")
     print(" ")
     print("Masukkan angka untuk Matriks 1")
@@ -18,7 +18,11 @@ def penjumlahan():
     bl7 =int(input("masukan kolom anda 1: "))
     bl8 =int(input("masukan kolom anda 2: "))
     bl9 =int(input("masukan kolom anda 3: "))
-    matriks1 = ([(bl1),(bl2),(bl3)],[(bl4),(bl5),(bl6)],[(bl7),(bl8),(bl9)])
+    mat1 = [
+        [(bl1),(bl2),(bl3)],
+        [(bl4),(bl5),(bl6)],
+        [(bl7),(bl8),(bl9)]
+        ]
     os.system('cls')
 
     print("Masukkan angka untuk Matriks 2")
@@ -32,37 +36,101 @@ def penjumlahan():
     bl7 =int(input("masukan kolom anda 1: "))
     bl8 =int(input("masukan kolom anda 2: "))
     bl9 =int(input("masukan kolom anda 3: "))
-    matriks2 = ([(bl1),(bl2),(bl3)],[(bl4),(bl5),(bl6)],[(bl7),(bl8),(bl9)])
+    mat2 = [
+        [(bl1),(bl2),(bl3)],
+        [(bl4),(bl5),(bl6)],
+        [(bl7),(bl8),(bl9)]
+        ]
+    #mat3 = []
     os.system('cls')
 
     print("Matriks 1")
     for i in range (3):
-        print(matriks1[i])
-
+        print(mat1[i])
     print("\nMatriks 2")
     for i in range (3): 
-        print(matriks2[i])
+        print(mat2[i])
 
-    #for x in range(0, len(matriks1)):
-    #    for y in range(0, len(matriks1[0])):
-    #        print (matriks1[x][y] + matriks2[x][y], end=' ')
-    #    print
-
+    print("\nHasil Penjumlahan Matriks")
+    for x in range(0, len(mat1)):
+        jumlah = 0
+        row = []
+        for y in range(0, len(mat1[0])):
+            jumlah = mat1[x][y] + mat2[x][y]
+            row.append(jumlah)
+        print(row)
+    #    mat3.append(row)
+    #print(mat3)
+    
 #Pengurangan
 def pengurangan():
-    #print("=" * 90)
+    #print("=" * 60)
     print("Pengurangan")
     print(" ")
+    print("Masukkan angka untuk Matriks 1")
+    print(" ")
+    bl1 =int(input("masukan kolom anda 1: "))
+    bl2 =int(input("masukan kolom anda 2: "))
+    bl3 =int(input("masukan kolom anda 3: "))
+    bl4 =int(input("masukan kolom anda 1: "))
+    bl5 =int(input("masukan kolom anda 2: "))
+    bl6 =int(input("masukan kolom anda 3: "))
+    bl7 =int(input("masukan kolom anda 1: "))
+    bl8 =int(input("masukan kolom anda 2: "))
+    bl9 =int(input("masukan kolom anda 3: "))
+    mat1 = [
+        [(bl1),(bl2),(bl3)],
+        [(bl4),(bl5),(bl6)],
+        [(bl7),(bl8),(bl9)]
+        ]
+    os.system('cls')
+
+    print("Masukkan angka untuk Matriks 2")
+    print(" ")
+    bl1 =int(input("masukan kolom anda 1: "))
+    bl2 =int(input("masukan kolom anda 2: "))
+    bl3 =int(input("masukan kolom anda 3: "))
+    bl4 =int(input("masukan kolom anda 1: "))
+    bl5 =int(input("masukan kolom anda 2: "))
+    bl6 =int(input("masukan kolom anda 3: "))
+    bl7 =int(input("masukan kolom anda 1: "))
+    bl8 =int(input("masukan kolom anda 2: "))
+    bl9 =int(input("masukan kolom anda 3: "))
+    mat2 = [
+        [(bl1),(bl2),(bl3)],
+        [(bl4),(bl5),(bl6)],
+        [(bl7),(bl8),(bl9)]
+        ]
+    #mat3 = []
+    os.system('cls')
+
+    print("Matriks 1")
+    for i in range (3):
+        print(mat1[i])
+    print("\nMatriks 2")
+    for i in range (3): 
+        print(mat2[i])
+
+    print("\nHasil Pengurangan Matriks")
+    for x in range(0, len(mat1)):
+        jumlah = 0
+        row = []
+        for y in range(0, len(mat1[0])):
+            jumlah = mat1[x][y] - mat2[x][y]
+            row.append(jumlah)
+        print(row)
+    #    mat3.append(row)
+    #print(mat3)
 
 #Perkalian
 def perkalian():
-    #print("=" * 90)
+    #print("=" * 60)
     print("Perkalian")
     print(" ")
 
 #Transpose
 def transpose():
-    #print("=" * 90)
+    #print("=" * 60)
     print("Transpose")
     print(" ")
 
