98c338733e4e8023b0c331fdb33ee7defce23736
rafifadhila440 <rafifadhila440@gmail.com>
2022-11-13 16:22:54 +0700

    Program Transpose Matriks

--- aplikasi_matriks_PT10.py
+++ aplikasi_matriks_PT10.py
@@ -3,6 +3,7 @@
 
 #Penjumlahan
 import os
+#import numpy as np
 def penjumlahan():
     #print("=" * 60)    
     print("Penjumlahan matriks")
@@ -127,12 +128,102 @@ def perkalian():
     #print("=" * 60)
     print("Perkalian")
     print(" ")
+    print("Masukan angka untuk Matriks 1")
+    print("")
+    bl1 =int(input("masukan kolom anda 1: "))
+    bl2 =int(input("masukan kolom anda 2: "))
+    bl3 =int(input("masukan kolom anda 3: "))
+    bl4 =int(input("masukan kolom anda 1: "))
+    bl5 =int(input("masukan kolom anda 2: "))
+    bl6 =int(input("masukan kolom anda 3: "))
+    bl7 =int(input("masukan kolom anda 1: "))
+    bl8 =int(input("masukan kolom anda 2: "))
+    bl9 =int(input("masukan kolom anda 3: "))
+    mat1 = [
+        [(bl1),(bl2),(bl3)],
+        [(bl4),(bl5),(bl6)],
+        [(bl7),(bl8),(bl9)]
+        ]
+    os.system('cls')
+
+    print("Masukkan angka untuk Matriks 2")
+    print(" ")
+    bl1 =int(input("masukan kolom anda 1: "))
+    bl2 =int(input("masukan kolom anda 2: "))
+    bl3 =int(input("masukan kolom anda 3: "))
+    bl4 =int(input("masukan kolom anda 1: "))
+    bl5 =int(input("masukan kolom anda 2: "))
+    bl6 =int(input("masukan kolom anda 3: "))
+    bl7 =int(input("masukan kolom anda 1: "))
+    bl8 =int(input("masukan kolom anda 2: "))
+    bl9 =int(input("masukan kolom anda 3: "))
+    mat2 = [
+        [(bl1),(bl2),(bl3)],
+        [(bl4),(bl5),(bl6)],
+        [(bl7),(bl8),(bl9)]
+        ]
+    #mat3 = []
+    os.system('cls')
+
+    print("Matriks 1")
+    for i in range (3):
+        print(mat1[i])
+    print("\nMatriks 2")
+    for i in range (3):
+        print(mat2[i])
+
+    print("\nHasil Perkalian Matriks")
+    for x in range(0, len(mat1)):
+        jumlah = 0
+        row = []
+        for y in range(0, len(mat1[0])):
+            jumlah = mat1[x][y] * mat2[x][y]
+            row.append(jumlah)
+        print(row)
+    #    mat3.append(row)
+ #print(mat3)
 
 #Transpose
 def transpose():
     #print("=" * 60)
     print("Transpose")
     print(" ")
+    bl1 =int(input("masukan kolom anda 1: "))
+    bl2 =int(input("masukan kolom anda 2: "))
+    bl3 =int(input("masukan kolom anda 3: "))
+    bl4 =int(input("masukan kolom anda 1: "))
+    bl5 =int(input("masukan kolom anda 2: "))
+    bl6 =int(input("masukan kolom anda 3: "))
+    bl7 =int(input("masukan kolom anda 1: "))
+    bl8 =int(input("masukan kolom anda 2: "))
+    bl9 =int(input("masukan kolom anda 3: "))
+    #tanpa menggunakan library numpy
+    mat1 = [
+        [(bl1),(bl2),(bl3)],
+        [(bl4),(bl5),(bl6)],
+        [(bl7),(bl8),(bl9)]
+        ]
+    os.system('cls')
+
+    print("Matriks")
+    for i in range (3):
+        print(mat1[i])
+
+    result = [[mat1[j][i] for j in range(len(mat1))] for i in range(len(mat1[0]))]
+    print("\nHasil Transpose")
+    for r in result:
+       print(r)
+
+    #menggunakan library numpy
+    #mat1 = np.array([[bl1,bl2,bl3],
+    #                [bl4,bl5,bl6],
+    #                [bl7,bl8,bl9]])
+    #os.system('cls')
+    #print("Matriks")
+    #print(mat1)
+    #transpose_matriks = np.transpose(mat1)
+    #print("\nTranpose Matriks")
+    #print(transpose_matriks)
 
 kondisi = True
 
