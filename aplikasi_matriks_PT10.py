e6cf225ea0b197d65478169771544489b2406f01
rafifadhila440 <rafifadhila440@gmail.com>
2022-11-14 08:18:58 +0700

    Perbaikan Operasi Perkalian

--- aplikasi_matriks_PT10.py
+++ aplikasi_matriks_PT10.py
@@ -162,7 +162,7 @@ def perkalian():
         [(bl4),(bl5),(bl6)],
         [(bl7),(bl8),(bl9)]
         ]
-    #mat3 = []
+    mat3 = []
     os.system('cls')
 
     print("Matriks 1")
@@ -174,12 +174,26 @@ def perkalian():
 
     print("\nHasil Perkalian Matriks")
     for x in range(0, len(mat1)):
-        jumlah = 0
         row = []
         for y in range(0, len(mat1[0])):
-            jumlah = mat1[x][y] * mat2[x][y]
-            row.append(jumlah)
-        print(row)
+            total = 0
+            for z in range(0, len(mat1)):
+                total = total + (mat1[x][z] * mat2[z][y])
+            row.append(total)
+        mat3.append(row)
+    for x in range(0, len(mat3)):
+        for y in range(0, len(mat3[0])):
+            print (mat3[x][y], end=' ')
+        print ()
+
+    #print("\nHasil Perkalian Matriks")
+    #for x in range(0, len(mat1)):
+    #    jumlah = 0
+    #    row = []
+    #    for y in range(0, len(mat1[0])):
+    #        jumlah = mat1[x][y] * mat2[x][y]
+    #        row.append(jumlah)
+    #    print(row)
     #    mat3.append(row)
  #print(mat3)
 
